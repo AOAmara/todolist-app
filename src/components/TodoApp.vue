d0f45c553cd3ceb54f864c34e5ad193c8c372870
Adnan Amara <adnan.amara@jetteam.tnadnan.amara@jetteam.tn>
2021-08-22 15:14:20 +0100

    app ready

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container">
-    <h2 class="text-center">Todo list App</h2>
+    <h2 class="text-center mt-5">Todo list App</h2>
     <!-- Input -->
     <div class="d-flex">
-      <input type="text" placeholder="Enter todo item" class="form-control" v-model="task">
-      <button class="btn btn-warning rounded-0" @click="submitTask">SUBMIT</button>
+      <input type="text" placeholder="Add new task" class="form-control rounded-0" v-model="task">
+      <button class="btn btn-primary rounded-0" @click="submitTask">SUBMIT</button>
     </div>
     <!-- Tasks table -->
     <table class="table table-bordered mt-5">
@@ -12,21 +12,34 @@
       <tr>
         <th scope="col">Task</th>
         <th scope="col">Status</th>
-        <th scope="col" class="text-center">Update</th>
-        <th scope="col" class="text-center">Delete</th>
+        <th scope="col" class="text-center">#</th>
+        <th scope="col" class="text-center">#</th>
       </tr>
       </thead>
       <tbody>
       <tr v-for="(task, index) in tasks" :key="index">
-        <th>{{ task.name }}</th>
-        <td>{{ task.status }}</td>
+        <th>
+          <span :class="{'finished': task.status === 'finished'}">{{ task.name }}</span>
+        </th>
+        <td style="width: 120px">
+          <span class="pointer"
+                @click="changeStatus(index)"
+                :class="{
+                'text-primary': task.status === 'to-do',
+                'text-warning': task.status === 'in-progress',
+                'text-success': task.status === 'finished'
+                }"
+          >
+            {{ firstCharUpper(task.status) }}
+          </span>
+        </td>
         <td>
-          <div class="text-center" @click="updateTask(index)">
+          <div class="text-center text-info pointer" @click="updateTask(index)">
             <span class="fa fa-pen"></span>
           </div>
         </td>
         <td>
-          <div class="text-center" @click="removeTask(index)">
+          <div class="text-center text-danger pointer" @click="removeTask(index)">
             <span class="fa fa-trash"></span>
           </div>
         </td>
@@ -44,18 +57,19 @@ export default {
   },
   data() {
     return {
+      tasks: [],
       task: '',
       updatedTask: null,
-      tasks: [
-        {
-          name: 'Take out the trash.',
-          status: 'to-do'
-        },
-        {
-          name: 'Visit the dentist.',
-          status: 'in-progress'
-        }
-      ]
+      availableStatuses: ['to-do', 'in-progress', 'finished']
+    }
+  },
+  mounted() {
+    if(localStorage.getItem('tasks')) {
+      try {
+        this.tasks = JSON.parse(localStorage.getItem('tasks'));
+      } catch (e) {
+        localStorage.removeItem('tasks');
+      }
     }
   },
   methods: {
@@ -71,18 +85,39 @@ export default {
         this.updatedTask = null;
       }
       this.task = '';
+      this.saveToLocalStorage();
+    },
+    saveToLocalStorage() {
+      const parsed = JSON.stringify(this.tasks);
+      localStorage.setItem('tasks', parsed);
+    },
+    firstCharUpper(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
     },
     updateTask(index) {
       this.task = this.tasks[index].name;
       this.updatedTask = index;
     },
+    changeStatus(index) {
+      let newIndex = this.availableStatuses.indexOf(this.tasks[index].status);
+      if(++newIndex > 2) newIndex = 0;
+      this.tasks[index].status = this.availableStatuses[newIndex];
+    },
     removeTask(index) {
       this.tasks.splice(index, 1);
+      this.saveToLocalStorage();
     }
   }
 }
 </script>
 
 <style scoped>
-
+  .pointer {
+    cursor: pointer;
+  }
+  .finished {
+    color: gray;
+    font-weight: normal;
+    text-decoration: line-through;
+  }
 </style>
